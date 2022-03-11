b4ba8cac9bdab296326d56c3e7e77bcbc7a0f409
semerim <semerim@yahoo.com.br>
2022-03-11 10:45:08 -0300

    1.0.1

--- testeTelegram.php
+++ testeTelegram.php
@@ -7,6 +7,6 @@ session_start();
 require_once('conexao.php');
 require_once('inc_rastreamento.php');
 
-send_Telegram(utf8_decode("\n<b>" . 'Teste de Mensagem CABEÇA SÃO MÕES' . "</b>\n\n" . 'Somente um teste de çãoáíúóÚ'), TELEGRAM_RASTREAMENTO_BOT_TOKEN)
+send_Telegram(utf8_decode("\n<b>" . 'Teste de Mensagem CABEÇA SÃO MÕES' . "</b>\n\n" . 'Somente um teste 123 de çãoáíúóÚ'), TELEGRAM_RASTREAMENTO_BOT_TOKEN)
 
 ?>
